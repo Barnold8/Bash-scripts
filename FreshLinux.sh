53f53ba902aa1edf2c410313f5240eb8bfcbcb4f
Brandon Wright <55092542+Barnold8@users.noreply.github.com>
2022-12-25 04:08:17 +0000

    Create FreshLinux.sh
    Added install.sh for pacman package manager linux to make hopping distros nicer. keys are left out by default

---
+++ FreshLinux.sh
@@ -0,0 +1,73 @@
+#### Enable snapd
+cd Downloads
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+cd ../
+rm -rf snapd
+cd ../
+#### Enable snapd
+
+#### Docker
+yay -S docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo systemctl enable docker
+sudo systemctl start docker
+#### Docker
+
+
+#### pacman
+yes | sudo pacman -S xclip
+yes | sudo pacman -S vscode
+yes | sudo pacman -S neovimsudo 
+yes | sudo pacman -S steam
+yes | sudo pacman -S jre-openjdk
+yes | sudo pacman -S nodejs
+yes | sudo pacman -S npm
+yes | sudo pacman -S neofetch
+#### pacman
+
+#### yay
+yes | yay -S teams
+#### yay
+
+
+#### App images
+mkdir AppImages
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.1.9/Obsidian-1.1.9.AppImage
+#### App images
+
+
+#### Snap packs
+sudo snap install valgrind --classic
+sudo snap install intellij-idea-community --classic
+sudo snap install discord
+#### Snap packs
+
+
+#### bashrc
+echo 'alias Refresh="source ~/.bashrc "' >> .bashrc
+echo 'alias EditBashrc="nvim ~/.bashrc"' >> .bashrc
+echo 'alias pip="python3 -m pip"' >> .bashrc
+echo 'alias macos="neofetch --ascii_distro macos"' >> .bashrc
+echo 'alias win7="neofetch --ascii_distro windows7"' >> .bashrc
+echo 'alias win10="neofetch --ascii_distro windows10"' >> .bashrc
+echo 'alias GLPass="echo"' >> .bashrc
+echo 'alias GLPPass="echo "' >> .bashrc
+echo 'alias cls="clear"' >> .bashrc
+echo 'alias clsls="clear && ls"' >> .bashrc
+echo 'alias EditNvimrc="nvim ~/.config/nvim/init.lua"' >> .bashrc
+echo 'alias nvimLOC="echo '~/.config/nvim/'"' >> .bashrc
+echo 'alias py="python3" ' >> .bashrc
+echo 'alias clse="cls && exit"' >> .bashrc
+echo 'alias tViewer="teamviewer -daemon start && teamviewer"' >> .bashrc
+#### bashrc
+
+
+
+
