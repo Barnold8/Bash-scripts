12dca03cdcca4b80cc82c3beaa2b5f1347b23894
Brandon Wright <55092542+Barnold8@users.noreply.github.com>
2022-12-26 00:40:34 +0000

    Update FreshLinux.sh

--- FreshLinux.sh
+++ FreshLinux.sh
@@ -1,3 +1,5 @@
+
+
 #### Enable snapd
 cd Downloads
 git clone https://aur.archlinux.org/snapd.git
@@ -18,6 +20,8 @@ yes | sudo pacman -S vscode
 yes | sudo pacman -S neovim 
 sudo pacman -S steam
 yes | sudo pacman -S jre-openjdk
+yes | sudo pacman -S discord
+yes | sudo pacman -S valgrind
 yes | sudo pacman -S nodejs
 yes | sudo pacman -S npm
 yes | sudo pacman -S neofetch
@@ -48,8 +52,8 @@ echo 'alias pip="python3 -m pip"' >> .bashrc
 echo 'alias macos="neofetch --ascii_distro macos"' >> .bashrc
 echo 'alias win7="neofetch --ascii_distro windows7"' >> .bashrc
 echo 'alias win10="neofetch --ascii_distro windows10"' >> .bashrc
-echo 'alias GLPass="echo"' >> .bashrc
-echo 'alias GLPPass="echo "' >> .bashrc
+echo 'alias GLPass="echo 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"' >> .bashrc
+echo 'alias GLPPass="echo glpat-NFPMo2-Gy48h_yxxq8Ft"' >> .bashrc
 echo 'alias cls="clear"' >> .bashrc
 echo 'alias clsls="clear && ls"' >> .bashrc
 echo 'alias EditNvimrc="nvim ~/.config/nvim/init.lua"' >> .bashrc
@@ -59,6 +63,7 @@ echo 'alias clse="cls && exit"' >> .bashrc
 echo 'alias tViewer="teamviewer -daemon start && teamviewer"' >> .bashrc
 #### bashrc
 
+
 #### Teamviewer
 git clone https://aur.archlinux.org/teamviewer.git
 cd teamviewer
@@ -67,7 +72,6 @@ cd ../
 rm -rf teamviewer
 #### Teamviewer
 
-
 #### Docker
 yay -S docker
 sudo usermod -aG docker $USER
@@ -75,4 +79,7 @@ newgrp docker
 sudo systemctl enable docker
 sudo systemctl start docker
 #### Docker
+
+
+
 
